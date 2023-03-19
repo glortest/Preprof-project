14b5f1923cd2a03bafaa53123249995b7fe3370d
Gregory Omelchuk <gregoryomelchuk@vk.com>
2023-03-19 14:00:30 +0300

    add code in main_activity

--- app/src/main/java/com/example/preprof_project/MainActivity.java
+++ app/src/main/java/com/example/preprof_project/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.preprof_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bt1 = (Button) findViewById(R.id.button1);
+        Button bt2 = (Button) findViewById(R.id.button2);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // переключение на edit_data_activity
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // переключение на activity_main
+            }
+        });
     }
 }
